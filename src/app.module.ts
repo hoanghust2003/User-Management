a2da63a3130ba0371d672c2d78e0a0b7a8576deb
cowboiz <dovanhoang1201@gmail.com>
2024-11-05 13:48:42 +0700

    config .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,20 +7,23 @@ import { GroupModule } from './group/group.module';
 import { UserGroup } from './entities/user_group.entity';
 import { Group } from './entities/group.entity';
 import { GroupPermission } from './entities/group_permission.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // ConfigModule sẽ là module toàn cục
+    }), // Nạp các biến từ file .env
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DATABASE_HOST, // hoặc địa chỉ của MySQL server
-      port: parseInt(process.env.DATABASE_PORT, 10), // Chuyển đổi sang số
-      username: process.env.DATABASE_USER, // tên đăng nhập
-      password: process.env.DATABASE_PASSWORD, // mật khẩu
-      database: process.env.DATABASE_NAME, // tên cơ sở dữ liệu
-      entities: [User, UserGroup, Group, GroupPermission], // đường dẫn tới các entity
-      synchronize: false, 
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [User, UserGroup, Group, GroupPermission],
+      synchronize: true,
     }),
-
     UsersModule,
     AuthModule,
     GroupModule,
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
+
 
 declare const module: any;
 async function bootstrap() {
-  dotenv.config();
+
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT);
 
   if (module.hot) {
     module.hot.accept();
